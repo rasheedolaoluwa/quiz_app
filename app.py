5fe07f533f6b83feced3b253889214871d947ffb
Rasheed <radeyola@gmail.com>
2024-10-16 15:16:48 +0100

    Ability to create quiz

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'nigeriawillbegreat'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz_app.db'
 db = SQLAlchemy(app)
+
+# Flask-Login setup
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Define User Model
 class User(UserMixin, db.Model):
@@ -32,6 +41,61 @@ class AnswerChoice(db.Model):
     text = db.Column(db.String(150), nullable=False)
     is_correct = db.Column(db.Boolean, default=False)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+             flash('Invalid credentials. Please try again.')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Check if the user already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists. Please choose a different one.')
+        else:
+            # Create a new user
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful! Please log in.')
+            return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/create_quiz', methods=['GET', 'POST'])
+@login_required
+def create_quiz():
+    if request.method == 'POST':
+        title = request.form['title']
+        new_quiz = Quiz(title=title)
+        db.session.add(new_quiz)
+        db.session.commit()
+        flash('Quiz created successfully!')
+        return redirect(url_for('home'))
+    return render_template('create_quiz.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/')
+@login_required
+def home():
+    return "Welcome to the Interactive Quiz Application!"
+
 if __name__ == '__main__':
-    db.create_all()  # Create database tables
+    with app.app_context():
+        db.create_all()  # Create database tables
     app.run(debug=True)
